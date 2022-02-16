af7ab4297a7d78f02688a7974fc0ad8d6ef65b30
yaohongyun <1115703148@qq.com>
2022-02-16 10:16:51 +0800

    fix: fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,18 +8,20 @@ module.exports = {
   },
   env: {
     browser: true,
+    node: true,
+    es6: true,
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
+  extends: ['plugin:vue/recommended', 'eslint:recommended'],
   // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  // plugins: [
+  //   'html'
+  // ],
   // add your custom rules here
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
